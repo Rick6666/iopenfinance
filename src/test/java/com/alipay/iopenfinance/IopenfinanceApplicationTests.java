5015c38fade313656299ff4a1d5d4a87dceb75b4
wangjiapeng <wangjiapeng0921@163.com>
2022-05-24 23:12:11 +0800

    create the project

---
+++ src/test/java/com/alipay/iopenfinance/IopenfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alipay.iopenfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IopenfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
